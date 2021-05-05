9a656992f3ff6b3d1e02aac3dae9ad34a5bc6223
Danil Melentev <mejleht@yandex.ru>
2021-05-05 17:59:35 +0300

    feat: филдсет для оценки проведенного времени

---
+++ scripts/index.js
@@ -0,0 +1,58 @@
+// UI kit: Assessment
+const assessmentContainer = document.querySelector('.assessment');
+
+const assessmentDescription = assessmentContainer.querySelector('.assessment__description');
+
+const positiveInput = assessmentContainer.querySelector('#positive');
+const positiveInputEmoji = assessmentContainer.querySelector('.assessment__emoji_type_positive');
+
+const neutralInput = assessmentContainer.querySelector('#neutral');
+const neutralInputEmoji = assessmentContainer.querySelector('.assessment__emoji_type_neutral');
+
+const negativeInput = assessmentContainer.querySelector('#negative');
+const negativeInputEmoji = assessmentContainer.querySelector('.assessment__emoji_type_negative');
+
+// Точно есть более изящный способ!
+positiveInput.addEventListener('change', () => {
+  // Меняет фоновое изображение на активный смайлик
+  positiveInputEmoji.classList.add('assessment__emoji_type_positive-active');
+
+  // Удаляем фоновые изображения на остальных смайликах
+  neutralInputEmoji.classList.remove('assessment__emoji_type_neutral-active');
+  negativeInputEmoji.classList.remove('assessment__emoji_type_negative-active');
+
+  //Меняем текст описания
+  assessmentDescription.textContent = "Было классно!";
+
+  //Меняем цвет описания
+  assessmentDescription.classList.add('assessment__description_type_positive')
+  assessmentDescription.classList.remove('assessment__description_type_neutral')
+  assessmentDescription.classList.remove('assessment__description_type_negative')
+})
+
+neutralInput.addEventListener('change', () => {
+  neutralInputEmoji.classList.add('assessment__emoji_type_neutral-active');
+
+  positiveInputEmoji.classList.remove('assessment__emoji_type_positive-active');
+  negativeInputEmoji.classList.remove('assessment__emoji_type_negative-active');
+
+  assessmentDescription.textContent = "Нормально";
+
+  assessmentDescription.classList.remove('assessment__description_type_positive')
+  assessmentDescription.classList.add('assessment__description_type_neutral')
+  assessmentDescription.classList.remove('assessment__description_type_negative')
+})
+
+negativeInput.addEventListener('change', () => {
+  negativeInputEmoji.classList.add('assessment__emoji_type_negative-active');
+
+  positiveInputEmoji.classList.remove('assessment__emoji_type_positive-active');
+  neutralInputEmoji.classList.remove('assessment__emoji_type_neutral-active');
+
+  assessmentDescription.textContent = "Что-пошло не так";
+
+  assessmentDescription.classList.remove('assessment__description_type_positive')
+  assessmentDescription.classList.remove('assessment__description_type_neutral')
+  assessmentDescription.classList.add('assessment__description_type_negative')
+})
+// /Assessment
