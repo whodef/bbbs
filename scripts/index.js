baccd2dda473bd9b5c2c130731a5a9ebe09749c8
Danil Melentev <mejleht@yandex.ru>
2021-05-08 10:32:49 +0300

    feat: появление меню на обратный скролл

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
-//Всплывающее окно в меню
-//Заменил обычный .menu__item:hover sub-menu на JS.
-//Всё ради БЭМ!
 (function(){
+  // =========
+  //Всплывающее окно в меню
+  //Заменил обычный .menu__item:hover sub-menu на JS.
+  //Всё ради БЭМ!
   const menuContainer = document.querySelector('.menu__container');
   const menuItemWhithSubMenu = menuContainer.querySelector('.menu__item-with-sub-menu');
   const subMenu = menuItemWhithSubMenu.querySelector('.sub-menu');
@@ -18,11 +19,12 @@
       subMenu.classList.remove('sub-menu_visible')
     }
   })
-})();
-// /Всплывающее окно в меню
+  // /Всплывающее окно в меню
+  // =========
 
-//Нажатие по бургеру
-(function(){
+
+  // =========
+  // Нажатие по бургеру
   const burgerBtn = document.querySelector('.header__button_type_burger-menu');
   const headerMenuContainer = document.querySelector('.menu__container');
 
@@ -31,11 +33,14 @@
     burgerBtn.classList.toggle('header__button_type_burger-menu');
     burgerBtn.classList.toggle('header__button_type_close-menu');
   })
-})();
-// /Нажатие по бургеру
+  // /Нажатие по бургеру
+  // =========
 
-// На обратный скролл меню появляется
-(function(){
+
+  // =========
+  // На обратный скролл меню появляется
+  const toggleHeaderHeight = 400; // высота, после которой эта фича начинает работать
+
   const header = document.querySelector('.header');
 
   let currentYpos = window.pageYOffset;
@@ -43,14 +48,19 @@
   document.addEventListener('scroll', () => {
     currentYpos = window.pageYOffset;
 
-    // if (currentYpos < prevYpos) {
-    //   header.classList.add('header_fixed');
-    // }
-    // else {
-    //   header.classList.remove('header_fixed');
-    // }
+    if (currentYpos < prevYpos && currentYpos > toggleHeaderHeight) {
+      header.classList.add('header_fixed');
+    }
+    else {
+      header.classList.remove('header_fixed');
+      // Также здесь можно убрать меню бургер, если оно будет открыто:
+      headerMenuContainer.classList.remove('menu__container_opened');
+      burgerBtn.classList.add('header__button_type_burger-menu');
+      burgerBtn.classList.remove('header__button_type_close-menu');
+    }
 
     prevYpos = currentYpos;
   })
+  // /На обратный скролл меню появляется
+  // =========
 })();
-// На обратный скролл меню появляется
